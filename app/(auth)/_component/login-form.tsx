e87c9ef64c8483cb87a9cb638669ee3b47b473e8
Rijan Regmi <rijanregmi111@gmail.com>
2025-12-27 12:45:09 +0545

    create loginform

---
+++ app/(auth)/_component/login-form.tsx
@@ -0,0 +1,28 @@
+"use client";
+import Link from "next/link";
+import AuthLayout from "./AuthLayout";
+
+export default function LoginForm() {
+  return (
+    <AuthLayout
+      title="Sign in"
+      switchText="Don't have an account?"
+      switchLink="/register"
+      switchLabel="Sign up"
+    >
+      <input className="auth-input" placeholder="Email or Phone Number" />
+      <input type="password" className="auth-input" placeholder="Password" />
+
+      <Link rel="stylesheet" href="/dashboard">
+        <button className="auth-btn">Sign in</button>
+      </Link>
+
+      <div className="auth-row">
+        <label>
+          <input type="checkbox" /> Remember me
+        </label>
+        <span>Forgot Password</span>
+      </div>
+    </AuthLayout>
+  );
+}
