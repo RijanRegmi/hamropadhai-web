c5914ce7caa95e6dac01e8c5afdc7d4b6cf16d6b
Rijan Regmi <rijanregmi111@gmail.com>
2025-12-27 12:46:45 +0545

    create register page

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import RegisterForm from "./../_component/register-form";
+
+export default function Page() {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+}
